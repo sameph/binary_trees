f9d6e7c36965902befd7982b98ce963d72997dce
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-08-30 00:49:41 -0700

    Task 0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary-tree node
+ * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node of the node to create
- * @value: value to put in the new-> node
- *  done by temesgen abdissa and samue dgneh
- * Return: Point to the newly created-> node
- *         NULL on the  failure
+ * @value: value to put in the new node
+ *
+ * Return: Pointer to the newly created node
+ *         NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -20,4 +20,4 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new->left = NULL;
 	new->right = NULL;
 	return (new);
-}
+}}
