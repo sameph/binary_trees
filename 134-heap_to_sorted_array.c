a498e5fcd900a4c5370251e193457dbcc55ac43e
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-08-31 05:26:31 -0700

    Advanced Tasks

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - converts a Binary Max_Heap
+ * to a sorted array of integer
+ * @heap: a pointer to  root node of the heap to convert
+ * @size:  address to store the size of the array
+ *
+ * Return: the generated array
+ *         NULL on failure
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+
+}
