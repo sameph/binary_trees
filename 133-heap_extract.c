775679702d42d46bc62bfd8958349165231d88f1
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-08-31 05:23:49 -0700

    Advanced Tasks

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root node of Max Binary_Heap
+ * @root: a double pointeroot node of heap
+ *
+ * Return: the value stored in the root node
+ *         0 on failure
+ */
+int heap_extract(heap_t **root)
+{
+
+}
